d9854aeb36cd7d4e4b898ea3eb907a528b774e0b
Jenisha E <jenisha.e@jmangroup.com>
2024-08-08 09:28:16 +0530

    controller changes

--- routes/expRoutes.js
+++ routes/expRoutes.js
@@ -3,8 +3,8 @@ const router=express.Router();
 const exproutes=require('../controller/expController');
 router.route('/')
 .get(exproutes.getExp)
-// .post(exproutes.postExp)
-// router.route('/:id')
-// .delete(exproutes.deleteExp)
-// .patch(exproutes.updateExp)
+.post(exproutes.postExp)
+router.route('/:id')
+.delete(exproutes.deleteExp)
+.patch(exproutes.updateExp)
 module.exports = router;
